54ff25c7564549c4c962056c79bd583544c6984a
ESTELA RUFINO BRITO <25203670@CFP127ALUNO.LOCAL>
2026-01-29 14:18:11 -0300

    Enviando anotações da da aula-02

--- Aula-02/app.js
+++ Aula-02/app.js
@@ -2,5 +2,34 @@
     * Objetivo: Projeto para realizar o calculo de médias escolares
     * Autor: Estela
     * Data: 29/01/2026
-    * Versão: 1.0
+    * Versão: 1.0.1.26
  */
+
+/*
+
+    Tipo de criação de variáveis
+
+    var -> permite criar um espaço em mem[oria do tipo variável.
+            Essa forma de criação hoje é consderada mais antiga,
+            é provavel que seja encontrada apenas em projetos
+            mais antigos
+        DICA: Caso você precise utilizar o var, recomenda-se
+            que seja uilizada em escopo global.
+            (significa no inicio de tudo disponivel para todos "public")
+            (escopo local seria dentro de um bloco "privete")
+
+    let -> Permite criar um espaço na memória do computador do tipo variável.
+            Essa forma de criação é realizada somente no escopo local,
+            ou seja dentro de um bloco de programação { aqui que irá colocar}.
+            Esse tipo de variável deixa de existir ao término do bloco.
+
+    cont -> Permite criar um espaço em memória do tipo constante,
+            ou seja, esse conteúdo não poderá sofrer mudanças durante
+            o projeto. 
+            !não deve muda a cont então não mude!
+                Observação: a const pode ser criada sem nenhum
+            problema no escopo global ou local, só veja se vale a pena.
+                Dica: Sempre que puder você pode criar essa const
+            escrita em MAIUSCULO para facilitar a sua utilização.
+
+*/
