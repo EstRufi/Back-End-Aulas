51b483c59dc23765e8dd66826e1c649dea824b63
ESTELA RUFINO BRITO <25203670@CFP127ALUNO.LOCAL>
2026-02-06 16:17:55 -0300

    fazendo o desafio bem legal

--- Atividade - Estela/app.js
+++ Atividade - Estela/app.js
@@ -14,67 +14,118 @@ const entradaDeDados = readline.createInterface({
     output: process.stdout
 
 })
-console.log("**********************************")
-console.log("Bem vindo(a) a Viva Moda")
-console.log("**********************************")
-entradaDeDados.question("Por favor digite o nome do comprador:", function(nome){
+console.log(`**********************************
+    \nBem vindo(a) a Viva Moda
+    \n**********************************`)
+
+
+entradaDeDados.question("Por favor digite o nome do comprador:", function (nome) {
 
     let nomeCliente = nome
 
-    entradaDeDados.question(`Obrigado(a) ${nomeCliente}, agora me diga qual o nome do produto:`, function(nomeProduto){
+    entradaDeDados.question(`Obrigado(a) ${nomeCliente}, agora me diga qual o nome do produto:`, function (nomeProduto) {
 
         let produto = nomeProduto
 
-        entradaDeDados.question(`${nomeCliente}, quanto pagou pelo(a) ${produto}:`, function(precoProduto){
+        entradaDeDados.question(`${nomeCliente}, quanto pagou pelo(a) ${produto}:`, function (precoProduto) {
 
             let valor = precoProduto
-    
-            entradaDeDados.question(`Senhor(a) ${nomeCliente}, me fale quanto foi a taxa de juros cobrado pelo produto ${produto}:`, function(taxaPorcentagem){
+
+            entradaDeDados.question(`Senhor(a) ${nomeCliente}, me fale quanto foi a taxa de juros cobrado pelo produto ${produto}:`, function (taxaPorcentagem) {
 
                 let taxinha = taxaPorcentagem
-                let taxa = taxinha/100
-        
-                entradaDeDados.question(`Para finalizarmos o calculo, senhor(a) ${nomeCliente}, você irá parcelar em quantas vezes?:`, function(Parcela){
-    
-                    let tempo = Parcela
-                    let verificando
+                let taxa = taxinha / 100
 
-                    // validação
-                    if (nomeCliente == "" || produto == "" || valor == "" || taxa == "" || tempo == ""){
+                //mes
+                entradaDeDados.question(`${nomeCliente}, você irá pagar em anos ou meses?`, function (AnoMes) {
+                    let ano = AnoMes
+                    if (ano = "anos") {
+                        entradaDeDados.question(`Para finalizarmos o calculo, senhor(a) ${nomeCliente}, você irá parcelar em quantas vezes?:`, function (Parcela) {
 
-                        verificando = "Você esqueceu de responder algo, olhe novamente"
-                        console.log(verificando)
-                    }
-                    else if(isNaN(valor) || isNaN(taxa) || isNaN(tempo)){
-                        
-                        verificando = "Eu só consigo ler números, não coloque uma letra. Faça novamente!!"
-                        console.log(verificando)
+                            let tempo = Parcela
+                            let verificando
+
+                            // validação
+                            if (nomeCliente == "" || produto == "" || valor == "" || taxa == "" || tempo == "" || ano == "") {
+
+                                verificando = "Você esqueceu de responder algo, olhe novamente"
+                                console.log(verificando)
+                            }
+                            else if (isNaN(valor) || isNaN(taxa) || isNaN(tempo)) {
+
+                                verificando = "Eu só consigo ler números, não coloque uma letra. Faça novamente!!"
+                                console.log(verificando)
+                            } else if (!isNaN(nomeCliente) || !isNaN(ano)) {
+                                verificando = "Coloque seu nome com apenas letras"
+                                console.log(verificando)
+                            }
+
+                            else {
+                                let anoTempo = tempo * 12
+                                let calculoTaxa = ((1 + Number(taxa)) ** Number(anoTempo))
+                                let montante = (Number(valor) * Number(calculoTaxa))
+                                let valorTaxa = Number(montante) - Number(valor)
+
+                                console.log(`
+                                ******************* Viva Moda *******************\n
+                                Muito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.
+                                A compra do produto ${produto}, tem um valor de: ${valor}.
+                                A sua compra será parcelada em ${anoTempo} vezes e o Sr(a) pagará: ${montante.toFixed(2)}.
+                                O acréscimo realizado ao valor de: ${valor} será de ${valorTaxa.toFixed(2)}.\n
+                                ******************* Viva Moda *******************`)
+
+                            }
+
+
+
+                        })
                     }
                     else {
+                        entradaDeDados.question(`Para finalizarmos o calculo, senhor(a) ${nomeCliente}, você irá parcelar em quantas vezes?:`, function (Parcela) {
 
-                        let calculoTaxa = ((1+ Number(taxa)) ** Number(tempo))
-                        let montante = (Number(valor) * Number(calculoTaxa))
-                        let valorTaxa = Number(montante) - Number(valor)
-                        
-                        console.log(verificando)
-                        console.log(`
-                            ******************* Viva Moda *******************\n
-                            Muito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.
-                            A compra do produto ${produto}, tem um valor de: ${valor}.
-                            A sua compra será parcelada em ${tempo} vezes e o Sr(a) pagará: ${montante.toFixed(2)}.
-                            O acréscimo realizado ao valor de: ${valor} será de ${valorTaxa.toFixed(2)}.\n
-                            ******************* Viva Moda *******************`)
-        
+                            let tempo = Parcela
+                            let verificando
+
+                            // validação
+                            if (nomeCliente == "" || produto == "" || valor == "" || taxa == "" || tempo == "") {
+
+                                verificando = "Você esqueceu de responder algo, olhe novamente"
+                                console.log(verificando)
+                            } else if (!isNaN(nomeCliente) || !isNaN(ano)) {
+
+                                verificando = "Coloque seu nome com somente letras"
+                                console.log(verificando)
+                            }
+                            else if (isNaN(valor) || isNaN(taxa) || isNaN(tempo)) {
+
+                                verificando = "Eu só consigo ler números, não coloque uma letra. Faça novamente!!"
+                                console.log(verificando)
+                            }
+                            else {
+
+                                let calculoTaxa = ((1 + Number(taxa)) ** Number(tempo))
+                                let montante = (Number(valor) * Number(calculoTaxa))
+                                let valorTaxa = Number(montante) - Number(valor)
+
+                                console.log(verificando)
+                                console.log(`
+                                ******************* Viva Moda *******************\n
+                                Muito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.
+                                A compra do produto ${produto}, tem um valor de: ${valor}.
+                                A sua compra será parcelada em ${tempo} vezes e o Sr(a) pagará: ${montante.toFixed(2)}.
+                                O acréscimo realizado ao valor de: ${valor} será de ${valorTaxa.toFixed(2)}.\n
+                                ******************* Viva Moda *******************`)
+
+                            }
+                        })
                     }
-
-                   
 
                 })
 
             })
 
         })
-       
+
     })
-    
+
 })
