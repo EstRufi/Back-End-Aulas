764a8979bf656770f52239733c55988dd4c3dfa2
ESTELA RUFINO BRITO <25203670@CFP127ALUNO.LOCAL>
2026-03-05 13:49:20 -0300

    finalizando arquivo formulario

--- Atividade - 03/02 - Media/modulo/formulario.js
+++ Atividade - 03/02 - Media/modulo/formulario.js
@@ -3,11 +3,11 @@ const formular = function (nomeAluno, nomeProf,
     nomeCurso, nomeDisciplina,
     clasificarMedia,resultadoMedia, n1, n2,
     n3, n4) {
-    let aluno = String(nomeAluno)
-    let prof = String(nomeProf)
-    let profS = String(sProf)
-    let alunoS = String(sAluno)
-    let curso = String(nomeCurso)
+    let aluno = String(nomeAluno).toUpperCase()
+    let prof = String(nomeProf).toUpperCase()
+    let profS = String(sProf).toLowerCase()
+    let alunoS = String(sAluno).toLowerCase()
+    let curso = String(nomeCurso).toLowerCase()
     let disciplina = String(nomeDisciplina)
     let clasificaMedia = String(clasificarMedia)
     let resultMedia = Number(resultadoMedia)
@@ -17,32 +17,32 @@ const formular = function (nomeAluno, nomeProf,
     let nota4 = Number(n4)
     let status
 
-    if (alunoS == `Feminino`.toLowerCase && profS == `Feminino`.toLowerCase) {
-        status = `A aluna ${aluno} foi ${clasificaMedia} na disciplina ${disciplina}\n
-        Curso: ${curso}\n
-        Professora: ${prof}\n
-        Notas da aluna: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3} e 4º foi ${nota4}\n
+    if (alunoS == `feminino` && profS == `feminino`) {
+        status = `A aluna ${aluno} foi ${clasificaMedia} na disciplina ${disciplina}
+        Curso: ${curso}
+        Professora: ${prof}
+        Notas da aluna: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3} e 4º foi ${nota4}
         Média final: ${resultMedia}`
     }
-    else if (alunoS == `Feminino`.toUpperCase && profS == `Masculino`.toLoCase) {
+    else if (alunoS == `feminino` && profS == `masculino`) {
         status = `A aluna ${aluno} foi ${clasificaMedia} na disciplina ${disciplina}
         Curso: ${curso}
         Professor: ${prof}
         Notas da aluna: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3} e 4º foi ${nota4}
         Média final: ${resultMedia}`
     }
-    else if (alunoS == `Masculino`.toLowerCase && profS == `Feminino`.toLowerCase) {
-        status = `O aluno ${aluno} foi ${clasificaMedia} na disciplina ${disciplina}\n
-        Curso: ${curso}\n
-        Professora: ${prof}\n
-        Notas do aluno: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3} e 4º foi ${nota4}\n
+    else if (alunoS == `masculino` && profS == `feminino`) {
+        status = `O aluno ${aluno} foi ${clasificaMedia} na disciplina ${disciplina}
+        Curso: ${curso}
+        Professora: ${prof}
+        Notas do aluno: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3} e 4º foi ${nota4}
         Média final: ${resultMedia}`
     }
-    else {
-        status = `O ${aluno} foi ${clasificaMedia} na disciplina ${disciplina}\n
-        Curso: ${curso}\n
-        Professor: ${prof}\n
-        Notas do aluno: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3} e 4º foi ${nota4}\n
+    else if (alunoS == `masculino` && profS == `masculino`){
+        status = `O ${aluno} foi ${clasificaMedia} na disciplina ${disciplina}
+        Curso: ${curso}
+        Professor: ${prof}
+        Notas do aluno: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3} e 4º foi ${nota4}
         Média final: ${resultMedia}`
     }
 
@@ -51,7 +51,7 @@ const formular = function (nomeAluno, nomeProf,
 
 const formularExame = function (nomeAluno, nomeProf,
     sProf, sAluno,
-    nomeCurso, nomeDisciplina,
+    nomeCurso, nomeDisciplina,resultadoMedia,
     clasificarExame,resultadoExame, n1, n2,
     n3, n4) {
     let aluno = String(nomeAluno)
@@ -60,6 +60,7 @@ const formularExame = function (nomeAluno, nomeProf,
     let alunoS = String(sAluno)
     let curso = String(nomeCurso)
     let disciplina = String(nomeDisciplina)
+    let resultMedia = Number(resultadoMedia)
     let clasificaExame = String(clasificarExame)
     let resultExame = Number(resultadoExame)
     let nota1 = Number(n1)
@@ -68,27 +69,27 @@ const formularExame = function (nomeAluno, nomeProf,
     let nota4 = Number(n4)
     let status
     
-    if (alunoS == `Feminino`.toLowerCase && profS == `Feminino`.toLowerCase) {
-        status = `A aluna ${aluno} foi ${clasificaExame} na disciplina ${disciplina}\n
-        Curso: ${curso}\n
-        Professora: ${prof}\n
-        Notas da aluna: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3}, 4º foi ${nota4} e nota do exame foi ${resultExame}\n
+    if (alunoS == `feminino` && profS == `feminino`) {
+        status = `A aluna ${aluno} foi ${clasificaExame} na disciplina ${disciplina}
+        Curso: ${curso}
+        Professora: ${prof}
+        Notas da aluna: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3}, 4º foi ${nota4} e nota do exame foi ${resultExame}
         Média final: ${resultMedia}
         Média final do Exame: ${resultExame}`
     }
-    else if (alunoS == `Feminino`.toLowerCase && profS == `Masculino`.toLowerCase) {
-        status = `A aluna ${aluno} foi ${clasificaExame} na disciplina ${disciplina}\n
-        Curso: ${curso}\n
-        Professor: ${prof}\n
-        Notas da aluna: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3}, 4º foi ${nota4} e nota do exame foi ${resultExame}\n
+    else if (alunoS == `feminino` && profS == `masculino`) {
+        status = `A aluna ${aluno} foi ${clasificaExame} na disciplina ${disciplina}
+        Curso: ${curso}
+        Professor: ${prof}
+        Notas da aluna: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3}, 4º foi ${nota4} e nota do exame foi ${resultExame}
         Média final: ${resultMedia}
         Média final do Exame: ${resultExame}`
     }
-    else if (alunoS == `Masculino`.toLowerCase && profS == `Feminino`.toLowerCase) {
-        status = `O aluno ${aluno} foi ${clasificaExame} na disciplina ${disciplina}\n
-        Curso: ${curso}\n
-        Professora: ${prof}\n
-        Notas do aluno: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3}, 4º foi${nota4} e nota do exame foi ${resultExame}\n
+    else if (alunoS == `masculino` && profS == `feminino`) {
+        status = `O aluno ${aluno} foi ${clasificaExame} na disciplina ${disciplina}
+        Curso: ${curso}
+        Professora: ${prof}
+        Notas do aluno: 1º foi ${nota1}, 2º foi ${nota2}, 3º foi ${nota3}, 4º foi${nota4} e nota do exame foi ${resultExame}
         Média final: ${resultMedia}
         Média final do Exame: ${resultExame}`
     }
@@ -103,13 +104,14 @@ const formularExame = function (nomeAluno, nomeProf,
 
     return status
 }
-
+/*
 const sa = "feminino"
-const sp = "masculino"
+const sp = "feminino"
 const na = "Ana"
 const np = "fe"
 const d = "matematica"
 const c = "ds"
+const rm = 90
 const cl = "aprovado"
 const r = 100
 const n1 = 100
@@ -117,4 +119,6 @@ const n2 = 100
 const n3 = 100
 const n4 = 100
 
-console.log(formular(na,np,sp,sa,c,d,cl,r,n1,n2,n3,n4))
+console.log(formularExame(na,np,sp,sa,c,d,rm,cl,r,n1,n2,n3,n4))
+
+*/
