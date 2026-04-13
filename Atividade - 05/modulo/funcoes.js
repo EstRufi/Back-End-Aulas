4edc9d65f072c1765376f417481c96afc19222e0
ESTELA RUFINO BRITO <25203670@CFP127ALUNO.LOCAL>
2026-04-13 15:48:28 -0300

    enviando a função da atividade 5 finalizada, faltando só a validação

--- Atividade - 05/modulo/funcoes.js
+++ Atividade - 05/modulo/funcoes.js
@@ -1,7 +1,7 @@
 const contato = require("./contatos.js")
 const zapContato = contato.contatos["whats-users"]
 
-// funciono
+// funciono não há filtro
 const getListaUsuario = function(){
 
     let tudoContatoArray = [zapContato]
@@ -9,7 +9,7 @@ const getListaUsuario = function(){
     return tudoContatoArray
 }
 
-// fazendo
+// funcionando está filtrando pelo numero de usuario ex (11987876567)
 const getListaDadosUsuario = function(numeroUsuario){
     let profileUsuarioArray = []
 
@@ -31,4 +31,92 @@ const getListaDadosUsuario = function(numeroUsuario){
     return profileUsuarioArray
 }
 
-console.log(getListaDadosUsuario(11987876567))
+// funcionando está filtrando pelo numero de usuario ex (11987876567)
+const getlistaContatosUsuario = function(numeroUsuario){
+    let listaContatoUsuarioArray = []
+
+    zapContato.forEach(function(itensContato){
+
+        if(Number(numeroUsuario) === Number(itensContato.number)){
+            itensContato.contacts.forEach(function(informacaoContato){
+                
+                descricaoContato = {
+                    nome: informacaoContato.name,
+                    foto: informacaoContato.image,
+                    descricao: informacaoContato.description
+                }
+                listaContatoUsuarioArray.push(descricaoContato)
+            })
+            
+        }
+        
+    })
+    return listaContatoUsuarioArray
+}
+
+// funcionando está filtrando pelo numero de usuario ex (11987876567)
+const getListaMensagensUsuario = function (numeroUsuario){
+    let listaMensagensUsuarioArray = []
+
+    zapContato.forEach(function(itensMensagem){
+        if(Number(numeroUsuario) === Number(itensMensagem.number)){
+            itensMensagem.contacts.forEach(function(mensagemUsuario){
+                conversasUsuario = {
+                    mensagem: mensagemUsuario.messages
+                }
+            })
+            listaMensagensUsuarioArray.push(conversasUsuario)
+        }
+    })
+    return listaMensagensUsuarioArray
+}
+
+// funcionando está filtrando pelo numero de usuario e nome do contato ex (11987876567) e "Ana Maria"
+const getListaConversaUsuario = function(numeroUsuario,nomeContato){
+    let listaConversaUsuarioArray = []
+
+    zapContato.forEach(function(itensMensagem){
+        if(Number(numeroUsuario) === Number(itensMensagem.number)){
+            itensMensagem.contacts.forEach(function(mensagemComUsuario){
+                if(String(nomeContato) === String(mensagemComUsuario.name)){
+                    conversasContato = {
+                        nomeUsuario: itensMensagem.account,
+                        numeroUsuario: itensMensagem.number,
+                        contato: mensagemComUsuario.name,
+                        mensagem: mensagemComUsuario.messages
+                    }
+                    listaConversaUsuarioArray.push(conversasContato)
+                }
+            })
+            
+        }
+    })
+    return listaConversaUsuarioArray
+}
+
+// funcionando está filtrando por uma palavra da conversa
+const getListaPalavrasChaves = function(palavraUsuario){
+    let listaConversaArray = []
+    let filtro = String(palavraUsuario).toUpperCase()
+    
+
+    zapContato.forEach(function(localizarItens){
+        localizarItens.contacts.forEach(function(localizador){
+            localizador.messages.forEach(function(itemMensagem){
+                let mensagensMaiusculo = itemMensagem.content.toUpperCase()
+
+                if(mensagensMaiusculo.includes(filtro)){
+                    filtroConversas = {
+                        nomeUsuario: localizarItens.account,
+                        numeroUsuario: localizarItens.number,
+                        contato: localizador.name,
+                        mensagem: localizador.messages
+                    }
+                    listaConversaArray.push(filtroConversas)
+                }
+            })
+        })
+        
+    })
+    return listaConversaArray
+}
